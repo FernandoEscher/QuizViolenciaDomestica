db136f30f8f2dcc81dd5c6205206d58dc6fd8d7a
Fernando Irías Escher <fernandoiriase@gmail.com>
2013-01-26 14:18:51 -0600

    Primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuizViolenciaDomestica::Application.config.secret_token = '51562e3de006b895ca49d2be4e826429ee616473a28351dcfa6ac6e4d3ce5b288ca25748ec800f657df43eb980040fdac102d83cb2ae5e1e8148662841d5e03d'
